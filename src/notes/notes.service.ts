f3c23f51f9cf61ecc53481697f76d71237e6a84d
Pedro Martín <pmunnin@gmail.com>
2026-09-09 18:45:38 +0200

    Add create and findAll methods to NotesService with constructor injection

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Note } from './note.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+    constructor(
+        @InjectRepository(Note) private notesRepository: Repository<Note>,
+    ) {}
+
+    async findAll(): Promise<Note[]> {
+        return this.notesRepository.find();
+    }
+    
+    async create(title:string): Promise<Note> {
+        const note = this.notesRepository.create({title, done: false});
+        return this.notesRepository.save(note);
+    }
+}
